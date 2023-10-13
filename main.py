4aa057b58f2700416c9a8f8526176bf1cb82925e
Lukasz Warzecha <lukasz.warzecha@aptiv.com>
2023-10-13 16:34:24 +0200

    sprint_3

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-
+import random
 
 class Dragon:
     """
@@ -13,10 +13,11 @@ class Dragon:
     >>> print(wawelski)
     Wawelski is at x=20, y=0
     """
-    def __init__(self, name):
+    def __init__(self, name, /, *, x=0, y=0):
         self.name = name
         self.x = 0
         self.y = 0
+        self.health = random.randint(50, 100)
 
     def set_position(self, x, y):
         self.x = x
@@ -26,8 +27,18 @@ class Dragon:
         self.x += dx
         self.y += dy
 
+    def make_damage(self):
+        damage = random.randint(5, 20)
+        self.health -= damage
+
+    def take_damage(self, damage):
+        self.health -= damage
+
     def __str__(self):
-        return f"{self.name} is at x={self.x}, y={self.y}"
+        if self.health <= 0:
+            return f"{self.name} is dead\nGold dropped: {random.randint(1, 100)}\nx={self.x}, y={self.y}"
+        else:
+            return f"{self.name} is at x={self.x}, y={self.y}"
 
 
 wawelski = Dragon("Wawelski")
@@ -38,5 +49,13 @@ wawelski.move(-10, 0)
 wawelski.move(15, 0)
 wawelski.move(15, 5)
 wawelski.move(0, -5)
+wawelski.make_damage()
+wawelski.take_damage(10)
+wawelski.take_damage(5)
+wawelski.take_damage(3)
+wawelski.take_damage(2)
+wawelski.take_damage(15)
+wawelski.take_damage(25)
+wawelski.take_damage(75)
 print(wawelski)
 
